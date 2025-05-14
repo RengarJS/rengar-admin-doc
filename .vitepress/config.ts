e9e0f2c35e10e47e72092d74cba55a164c9b9663
ashuicoder <1249735120@qq.com>
2025-05-14 23:08:16 +0800

    chore: 添加favicon.ico并优化vitepress配置
    添加了网站的favicon.ico文件，并在.vitepress/config.ts中进行了格式优化，提升了代码的可读性

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vitepress";
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "rengar-admin文档",
+
   description: "开发文档",
   base: "/rengar-admin-doc/",
   markdown: {
